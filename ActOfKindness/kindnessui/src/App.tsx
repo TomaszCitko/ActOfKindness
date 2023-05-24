95a0944831b24f4390a06c4542aa439b3de8b9ad
MaciejTrebacz <maciekdmd90@gmail.com>
2023-05-24 14:20:14 +0200

    create-react-app add custom logo add link to github

---
+++ ActOfKindness/kindnessui/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './images/handshake.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Act-Of-Kindness
+        </p>
+        <a
+          className="App-link"
+          href="https:/github.com/CodecoolGlobal/el-proyecte-grande-sprint-1-csharp-Tomek-Boomer"
+        >
+          Our GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
