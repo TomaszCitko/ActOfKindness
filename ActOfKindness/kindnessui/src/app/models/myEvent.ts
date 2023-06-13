bde7deceea129853db1b2c330191a3eae278d215
MaciejTrebacz <maciekdmd90@gmail.com>
2023-06-13 15:15:36 +0200

    slowly making event list and dashboard

---
+++ ActOfKindness/kindnessui/src/app/models/myEvent.ts
@@ -0,0 +1,16 @@
+export interface MyEvent {
+    id: string
+    User_Id: string
+    Created_Time: string
+    Localization: string
+    Title:	string
+    Description: string
+    From_Date: string
+    To_Date: string
+    Done: string
+    Latitude: string
+    Longitude: string
+    Moderated: string
+    Type: string
+    Image: string
+}
