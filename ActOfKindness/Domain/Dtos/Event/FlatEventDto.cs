e1d056716291b96de0f798f0ce278a919278468d
MarcinSzkurlat <szkurlat.martin@gmail.com>
2023-06-15 14:37:32 +0200

    Add event dtos

---
+++ ActOfKindness/Domain/Dtos/Event/FlatEventDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Dtos.Event
+{
+    public class FlatEventDto
+    {
+        public string Localization { get; set; }
+        public bool IsOnline { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public string Image { get; set; }
+    }
+}
