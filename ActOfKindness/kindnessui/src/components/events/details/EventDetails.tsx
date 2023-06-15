9b711f56bff3c5aac64f790a9711fa3fbde0b052
TomaszCitko <citkotomasz@gmail.com>
2023-06-15 17:13:30 +0200

    event

--- ActOfKindness/kindnessui/src/components/events/details/EventDetails.tsx
+++ ActOfKindness/kindnessui/src/components/events/details/EventDetails.tsx
@@ -16,8 +16,8 @@ function EventDetails() {
         user_Id: '',
         created_Time: '',
         localization: '',
-        title:	'',
-        description: '',
+        title:	'Czy dobrze być skrybą?',
+        description: 'A, wie pan, moim zdaniem to nie ma tak, że dobrze, albo że niedobrze. Gdybym miał powiedzieć, co cenię w życiu najbardziej, powiedziałbym, że ludzi. Ludzi, którzy podali mi pomocną dłoń, kiedy sobie nie radziłem, kiedy byłem sam, i co ciekawe, to właśnie przypadkowe spotkania wpływają na nasze życie. Chodzi o to, że kiedy wyznaje się pewne wartości, nawet pozornie uniwersalne, bywa, że nie znajduje się zrozumienia, które by tak rzec, które pomaga się nam rozwijać. Ja miałem szczęście, by tak rzec, ponieważ je znalazłem, i dziękuję życiu! Dziękuję mu; życie to śpiew, życie to taniec, życie to miłość! Wielu ludzi pyta mnie o to samo: ale jak ty to robisz, skąd czerpiesz tę radość? A ja odpowiadam, że to proste! To umiłowanie życia. To właśnie ono sprawia, że dzisiaj na przykład buduję maszyny, a jutro – kto wie? Dlaczego by nie – oddam się pracy społecznej i będę, ot, choćby, sadzić... doć— m-marchew...',
         from_Date: '',
         to_Date: '',
         done: '',
@@ -33,19 +33,19 @@ function EventDetails() {
             <Card>
                 <Card.Content>
                     <Card.Header>{event.title}</Card.Header>
+                    <Card.Meta>Description</Card.Meta>
                     <Card.Description>
+                        {event.description}
+                    </Card.Description>
                     <br/>Localization: {event.localization}
                     <br/>Start date: {event.from_Date} End date: {event.to_Date}
                     <br/>Localization: {event.localization}
                     <br/>Created by: {event.user_Id}
                     <br/>Created on: {event.created_Time}
 
-                    {event.done ? <h2>This help event has been completed</h2> : <h2>This help event is still ongoing</h2>}
-                    </Card.Description>
-                    <Card.Meta>Description</Card.Meta>
-                    <Card.Description>
-                        {event.description}
-                    </Card.Description>
+                    {event.done ? <h5>This help event has been completed</h5> : <h5>This help event is still ongoing</h5>}
+
+
                 </Card.Content>
                 <Card.Content size='large' src={event.image} />
             </Card>
