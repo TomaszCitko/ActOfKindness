63580b7a7934945a1b965c4115405631f508f7be
TomaszCitko <citkotomasz@gmail.com>
2023-06-15 09:24:53 +0200

    Event details in progress

--- ActOfKindness/kindnessui/src/components/events/details/EventDetails.tsx
+++ ActOfKindness/kindnessui/src/components/events/details/EventDetails.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import {MyEvent} from "../../../app/models/myEvent";
+import {Card} from "semantic-ui-react";
+//import {observer} from "mobx-react-lite";
+//import avatar from "../../../images/user.png"
 
-function EventDetails() {
+interface Props {
+    event: MyEvent
+}
+
+function EventDetails({event}: Props) {
     return (
-        <div>Event Details</div>
+        <>
+
+                {/* <Card>
+                    <Card.Content>
+                        <Card.Header>{event.title}</Card.Header>
+                        <Card.Description>
+                        <br>Localization: {event.localization}</br>
+                        <br>Start date: {event.from_Date} End date: {event.to_Date} </br>
+                        <br>Localization: {event.localization} </br>
+                        <br>Created by: {event.user_Id}</br>
+                        <br>Created on: {event.created_Time}</br>
+                        {event.done && <h2>This help event has been completed</h2>}
+                        </Card.Description>
+                        <Card.Meta>Description</Card.Meta>
+                        <Card.Description>
+                            {event.description}
+                        </Card.Description>
+                     </Card.Content>
+                    <Card.Content size='large' src={event.image} />
+                </Card> */}
+</>
     );
 }
 
