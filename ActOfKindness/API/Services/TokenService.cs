17dfa4ec1358cb123076b951ae39df8567bba810
MaciejTrebacz <maciekdmd90@gmail.com>
2023-06-23 01:16:42 +0200

    added token service added scope and identity setup

---
+++ ActOfKindness/API/Services/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService
+{
+    private readonly IConfiguration _configuration;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        // adding claims to get active user or do something based of those claims
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Email, user.Email),
+        };
+
+        // creating key we gonna added to user after sucesfull login/reg
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["TokenKey"]));
+
+        // hashing our key 
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescription = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = credentials
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescription);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
