4b06ab2c352b4f9a8128d5f6a9b3f51808bd805a
MaciejTrebacz <maciekdmd90@gmail.com>
2023-06-26 11:26:13 +0200

    spelling error fix

--- ActOfKindness/Application/Dtos/User/RegisterDto.cs
+++ ActOfKindness/Application/Dtos/User/RegisterDto.cs
@@ -8,11 +8,10 @@ public class RegisterDto
     [Required] 
     public string Username { get; set; }
     [Required]
-    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{7,40}", ErrorMessage = "Stronger passport please")]
+    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{7,40}", ErrorMessage = "Stronger password please")]
     public string Password { get; set; }
     [Required] [EmailAddress] 
     public string Email { get; set; }
     [Required]
     public string Location { get; set; }
-
 }
