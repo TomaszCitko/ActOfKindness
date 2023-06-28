b15180eb105086e22237a6dd2f44fdd9d7736ff5
TomaszCitko <citkotomasz@gmail.com>
2023-06-28 14:01:54 +0200

    Event filter

---
+++ ActOfKindness/Domain/Models/EventFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class EventFilter
+    {
+        public AppUser? CreatedBy { get; set; }
+        public string? Localization { get; set; }
+        public bool? IsOnline { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? StartingDate { get; set; }
+        public DateTime? EndingDate { get; set; }
+        public EventType? Type { get; set; }
+    }
+}
