a7bcc88b8b58478d27a2b0677d226bd42e8e1ff1
MarcinSzkurlat <szkurlat.martin@gmail.com>
2023-07-03 12:11:37 +0200

    Added and configured Serilog

--- ActOfKindness/API/Controllers/EventController.cs
+++ ActOfKindness/API/Controllers/EventController.cs
@@ -1,5 +1,4 @@
 ﻿using Application.Dtos.Event;
-using Application.Dtos.User;
 using Application.Interfaces;
 using Domain.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -74,13 +73,13 @@ namespace API.Controllers
 
         [AllowAnonymous]
         [HttpGet("filter")]
-        public async Task<ActionResult<List<DetailsEventDto>>> GetFilteredModeratedEventsAsyn([FromBody] EventFilter filter)
+        public async Task<ActionResult<List<DetailsEventDto>>> GetFilteredModeratedEvents([FromBody] EventFilter filter)
         {
             return await _eventService.GetFilteredModeratedEventsAsync(filter);
         }
 
         [HttpGet("{id:guid}/participants")]
-        public async Task<ActionResult<List<ParticipantDto>> >GetParticipantsAsync(Guid id)
+        public async Task<ActionResult<List<ParticipantDto>>> GetParticipants(Guid id)
         {
             return await _eventService.ReturnParticipantsDto(id);
         }
