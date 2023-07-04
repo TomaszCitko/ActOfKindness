17083c30a7c267d487d873e02aaf8e3c119af956
TomaszCitko <citkotomasz@gmail.com>
2023-07-04 22:39:42 +0200

    Text field also works :)

--- ActOfKindness/kindnessui/src/components/events/dashboard/EventFilters.tsx
+++ ActOfKindness/kindnessui/src/components/events/dashboard/EventFilters.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import {Header, Menu, Item, Form, TextArea} from "semantic-ui-react";
+import React, { ChangeEvent, useState } from 'react';
+import {Form, Header, Icon, Input, Menu, Search, TextArea} from "semantic-ui-react";
 import { MyEventFilter } from '../../../app/models/Events/myEventFilter';
 import { Link } from 'react-router-dom';
 import EventStore from '../../../app/stores/eventStore';
 import { useStore } from '../../../app/stores/store';
 import { type } from 'os';
+import { values } from 'mobx';
 
 
 function EventFilters() {
     
     const { eventStore } = useStore();
     const { filteredList} = eventStore;
-    const handleItemClick = (property: keyof typeof eventStore.filteredList, value: string) => 
+    const handleSearch = (property: keyof typeof eventStore.filteredList, value: string) => 
     {
         eventStore.filteredList[property] = value;
         handleFilteredEvents(eventStore.filteredList);
@@ -21,18 +22,21 @@ function EventFilters() {
         console.log(JSON.parse(JSON.stringify(filteredList)));
         eventStore.loadFilteredEvents(JSON.parse(JSON.stringify(filteredList)));
     }
-    
+    const handleTextSearch = (event: ChangeEvent<HTMLInputElement>) =>
+    {
+        const propName = event.target.name == "title" ? "title" : "description";
+        const inputValue = event.target.value;
+        handleSearch(propName,inputValue)
+    }
     
     return (
         <>
             <Menu className={"menuDashboard"}  vertical size={"large"} >
                 <Header className={"headerMenuDashboard"} icon={"filter"} attached color={"black"} content={"Filters"}></Header>
-                    <Form>
-                        <TextArea placeholder='Tell us more' />
-                    </Form>
-                <Menu.Item content={'All Events'} name="allEvents" active={true} onClick={() => handleItemClick("type", "")}/>
-                <Menu.Item content={"Help others"} onClick={() => handleItemClick("type", "0")}/>
-                <Menu.Item content={"Receive help"} onClick={() => handleItemClick("type", "1")}/>
+                <Input name="title" placeholder="Title" onChange={handleTextSearch} />
+                <Menu.Item content={'All Events'} name="allEvents" active={true} onClick={() => handleSearch("type", "")}/>
+                <Menu.Item content={"Help others"} onClick={() => handleSearch("type", "0")}/>
+                <Menu.Item content={"Receive help"} onClick={() => handleSearch("type", "1")}/>
             </Menu>
         </>
     );
