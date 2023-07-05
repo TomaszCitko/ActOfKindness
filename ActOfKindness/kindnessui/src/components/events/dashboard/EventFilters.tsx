ff60e68ea4a830bf162bd3ed51c231cc22373dcc
TomaszCitko <citkotomasz@gmail.com>
2023-07-05 08:40:04 +0200

    Functional filtering on front as well

--- ActOfKindness/kindnessui/src/components/events/dashboard/EventFilters.tsx
+++ ActOfKindness/kindnessui/src/components/events/dashboard/EventFilters.tsx
@@ -19,21 +19,30 @@ function EventFilters() {
 
     }
     const handleFilteredEvents = (filteredList:MyEventFilter) => {
-        console.log(JSON.parse(JSON.stringify(filteredList)));
         eventStore.loadFilteredEvents(JSON.parse(JSON.stringify(filteredList)));
     }
     const handleTextSearch = (event: ChangeEvent<HTMLInputElement>) =>
     {
-        const propName = event.target.name == "title" ? "title" : "description";
+        const propName = event.target.name == "title" ? "title" : (event.target.name == "description" ? "description" : "localization");
         const inputValue = event.target.value;
         handleSearch(propName,inputValue)
+    }
+    const handleDateSearch = (event: ChangeEvent<HTMLInputElement>) =>
+    {
+        const inputValue = event.target.value
+        const dateToApi = `${inputValue.slice(6,10)}-${inputValue.slice(3,5)}-${inputValue.slice(0,2)}`;
+        const filterDate = inputValue == "" ? inputValue : dateToApi;
+        handleSearch("startingDate",filterDate)
     }
     
     return (
         <>
             <Menu className={"menuDashboard"}  vertical size={"large"} >
                 <Header className={"headerMenuDashboard"} icon={"filter"} attached color={"black"} content={"Filters"}></Header>
-                <Input name="title" placeholder="Title" onChange={handleTextSearch} />
+                <Input name="title" placeholder="Title" size="big" onChange={handleTextSearch} />
+                <Input name="description" placeholder="Description" size="big" onChange={handleTextSearch} />
+                <Input name="location" placeholder="Location" size="big" onChange={handleTextSearch} />
+                <Input name="startingDate" placeholder="Select Date   DD/MM/YYYY" size="big" onChange={handleDateSearch} />
                 <Menu.Item content={'All Events'} name="allEvents" active={true} onClick={() => handleSearch("type", "")}/>
                 <Menu.Item content={"Help others"} onClick={() => handleSearch("type", "0")}/>
                 <Menu.Item content={"Receive help"} onClick={() => handleSearch("type", "1")}/>
