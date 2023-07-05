7323148f92108a169542d25f352a858eb4936888
MarcinSzkurlat <szkurlat.martin@gmail.com>
2023-07-05 14:57:02 +0200

    hot fix (Deleting an event now also deletes participants of that event)

---
+++ ActOfKindness/Persistence/Migrations/20230705121510_Change_EventUser_Delete_Behavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_EventUser_Delete_Behavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventUser_Events_EventId",
+                table: "EventUser");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventUser_Events_EventId",
+                table: "EventUser",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventUser_Events_EventId",
+                table: "EventUser");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventUser_Events_EventId",
+                table: "EventUser",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id");
+        }
+    }
+}
