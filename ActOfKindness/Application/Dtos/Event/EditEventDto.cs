c64e4a1134b1181404e3860586b83f2068b2ae30
aneta-k <kusnierz.aneta@gmail.com>
2023-07-12 15:49:04 +0200

    Change methods to update Event to accept strings as dates

--- ActOfKindness/Application/Dtos/Event/EditEventDto.cs
+++ ActOfKindness/Application/Dtos/Event/EditEventDto.cs
@@ -6,8 +6,8 @@
         public bool IsOnline { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime StartingDate { get; set; }
-        public DateTime EndingDate { get; set; }
+        public string StartingDate { get; set; }
+        public string EndingDate { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
         public string Image { get; set; }
