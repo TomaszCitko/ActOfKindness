7d90907e1ea50639126b27f6b85336821e22602e
aneta-k <kusnierz.aneta@gmail.com>
2023-08-08 18:38:23 +0200

    Add backend dates validation

---
+++ ActOfKindness/Application/Validators/DateHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Application.Validators;
+
+public static class DateHelper
+{
+    public static bool BeAValidDate(string dateString)
+    {
+        return DateTime.TryParseExact(dateString, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+    }
+}
+
--- ActOfKindness/Application/Validators/EditEventDtoValidator.cs
+++ ActOfKindness/Application/Validators/EditEventDtoValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Dtos.Event;
 using FluentValidation;
+using System.Globalization;
 
 namespace Application.Validators;
 
@@ -10,12 +11,16 @@ public class EditEventDtoValidator : AbstractValidator<EditEventDto>
         RuleFor(x => x.Localization).Length(0, 30);
         RuleFor(x => x.Title).Length(0, 80);
         RuleFor(x => x.Description).Length(0, 2000);
-        //RuleFor(x => x.StartingDate)
-        //    .LessThan(x => x.EndingDate)
-        //    .WithMessage("'StartingDate' must be earlier than 'EndingDate'");
-        //RuleFor(x => x.EndingDate)
-        //    .GreaterThan(x => x.StartingDate)
-        //    .WithMessage("'EndingDate' must be later than 'StartingDate'");
+        RuleFor(x => x.StartingDate)
+            .Must(DateHelper.BeAValidDate)
+            .WithMessage("Invalid 'StartingDate' format. Should be 'dd/MM/yyyy'");
+        RuleFor(x => x.EndingDate)
+            .Must(DateHelper.BeAValidDate)
+            .WithMessage("Invalid 'EndingDate' format. Should be 'dd/MM/yyyy'")
+            .Must(date => DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture) >= DateTime.Now.Date)
+            .WithMessage("'EndingDate' must be later than the current date and time")
+            .Must((x, date) => DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture) >= DateTime.ParseExact(x.StartingDate, "dd/MM/yyyy", CultureInfo.InvariantCulture))
+            .WithMessage($"'EndingDate' must be later than 'StartingDate'");
     }
 }
 
