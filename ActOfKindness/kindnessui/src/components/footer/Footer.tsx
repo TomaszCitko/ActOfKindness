1d3e9aab07ec651a5e98432ed9e0835375f534f9
aneta-k <kusnierz.aneta@gmail.com>
2023-08-08 10:31:13 +0200

    Change footer design

--- ActOfKindness/kindnessui/src/components/footer/Footer.tsx
+++ ActOfKindness/kindnessui/src/components/footer/Footer.tsx
@@ -1,9 +1,7 @@
-import logo from "../../images/handshake.png"
-import {Grid, Segment, Card} from 'semantic-ui-react';
-import {observer} from "mobx-react-lite";
-import LoadingComponent from "../../app/common/LoadingComponent";
 import React from "react";
-import Swal from "sweetalert2";
+import logo from "../../images/handshake.png"
+import { Grid, Segment, Card, Icon } from 'semantic-ui-react';
+import {observer} from "mobx-react-lite";
 import {useStore} from "../../app/stores/store";
 
 function Footer(){
@@ -13,9 +11,57 @@ function Footer(){
     if (eventStore.loading) return <></>;
 
     return(
-        <div style={{marginTop:'100px', width: '100vw', background: '#525252', alignContent: 'center', margin: '0px 0px 0px 0px', padding: '20px 0px 0px 0px', position: "absolute"}}>
+        <div style={{width: '100vw', background: '#525252', alignContent: 'center', margin: '50px 0px 0px', padding: '20px 0px 0px', position: "absolute"}}>
+            <Grid divided='vertically'>
+                <Grid.Row centered>
+                    <Grid.Column width={2} verticalAlign="middle">
+                        <div style={{ paddingRight: '20px' }}>
+                            <img src={logo} className="App-logo"/>
+                        </div>
+                    </Grid.Column>
+                    <Grid.Column width={2}>
+                        <MemberCard name='Aneta Kuśnierz' github='aneta-k' linkedin=''/>
+                    </Grid.Column>
+                    <Grid.Column width={2}>
+                        <MemberCard name='Marcin Szkurłat' github='MarcinSzkurlat' linkedin=''/>
+                    </Grid.Column>
+                    <Grid.Column width={2}>
+                        <MemberCard name='Maciej Trębacz' github='MaciejTrebacz' linkedin='maciejtrebacz-12345'/>
+                    </Grid.Column>
+                        <Grid.Column width={2}>
+                    <MemberCard name='Tomasz Citko' github='Tomek-Boomer' linkedin='tomasz-citko'/>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
 
-  <Grid divided='vertically'>
+        <Segment className="footer-segments" textAlign='center' style={{margin: '0px 0px 10px 0px', padding: '0px 0px 0px 0px'}}>
+            &copy; {new Date().getFullYear()} Act of Kindness Web Application. All rights reserved. <br/>Thank you for visiting.
+        </Segment>
+
+        </div>
+    );
+}
+
+function MemberCard({name, github, linkedin}) {
+    return (
+    <Card>
+        <Card.Content>
+            <Card.Header content={name} />
+            <Card.Meta content='Full-Stack Developer' />
+            <Card.Description>
+                {/* <p><Icon name='mail' /> {email}</p> */}
+                <p><Icon name='github' /> <a href={`https://github.com/${github}`}>GitHub</a></p>
+                <p><Icon name='linkedin' /> <a href={`https://linkedin.com/in/${linkedin}`}>LinkedIn</a></p>
+            </Card.Description>
+        </Card.Content>
+    </Card>
+    );
+}
+
+export default observer(Footer);
+
+{/* <div style={{width: '100vw', background: '#525252', alignContent: 'center', margin: '50px 0px 0px', padding: '20px 0px 0px', position: "absolute"}}>
+            <Grid divided='vertically'>
     <Grid.Row columns={2}>
     <Grid.Column width={3} textAlign="right">
         <Segment className="footer-segments" style={{margin: '5px 0px 0px 0px', padding: '0px 0px 0px 0px'}}><img src={logo} height='200px' className="App-logo"/></Segment>
@@ -63,8 +109,4 @@ function Footer(){
             </Grid.Row>
         </Grid>
             <Segment className="footer-segments" style={{margin: '0px 0px 10px 0px', padding: '0px 0px 0px 0px'}}><center>Act of kindness Web Application. All rights reserved. Thank you for visiting.</center></Segment>
-        </div>
-    );
-}
-
-export default observer(Footer);
+        </div> */}
