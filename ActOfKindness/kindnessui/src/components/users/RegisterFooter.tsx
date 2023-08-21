4b06a2eee20481ecc353bfe9ff196bdfaf1b2427
aneta-k <kusnierz.aneta@gmail.com>
2023-08-21 16:10:00 +0200

    Add links in modal footer

--- ActOfKindness/kindnessui/src/components/users/RegisterFooter.tsx
+++ ActOfKindness/kindnessui/src/components/users/RegisterFooter.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { store } from '../../app/stores/store';
+import LoginForm from './LoginForm';
+import LoginFooter from './LoginFooter';
 
 function RegisterFooter() {
+    const handleLoginClick = () => {
+        store.modalStore.closeModal();
+        store.modalStore.openModal(
+            <LoginForm/>,
+            "Login to help others",
+            <LoginFooter/>
+        );
+    }
+
     return (
         <>
-            <div>By continuing you agree to Terms of Service and Privacy Policy</div>
-            <div>Already have an account? Login <span>&rarr;</span></div>
+            <div className='smallFooterText'>By continuing you agree to <u>Terms of Service</u> and <u>Privacy Policy</u></div>
+            <div>Already have an account? <a onClick={handleLoginClick} style={{ cursor: 'pointer', textDecoration: 'underline', color: '#525252' }}>Login</a> <span>&rarr;</span></div>
         </>    
     )
 }
