8273dd0cc09137560eecdabb861bd4cf5472b56e
aneta-k <kusnierz.aneta@gmail.com>
2023-08-24 08:42:38 +0200

    Add FluentValidation for RegisterDto

--- ActOfKindness/Application/Dtos/User/RegisterDto.cs
+++ ActOfKindness/Application/Dtos/User/RegisterDto.cs
@@ -1,17 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Application.Dtos.User;
+﻿namespace Application.Dtos.User;
 
 public class RegisterDto
 {
-    [Required] 
     public string Username { get; set; }
-    [Required]
-    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{7,40}", ErrorMessage = "Stronger password please")]
     public string Password { get; set; }
-    [Required] 
-    [EmailAddress] 
     public string Email { get; set; }
-    [Required]
     public string Nickname { get; set; }
 }
